9333b69f6be1f90118318c65bcbc0dfd4c7517fa
颜洪毅 <yhyzgn@gmail.com>
2023-08-25 16:53:56 +0800

    feat:重新设计

---
+++ compiler/lexer/token_test.go
@@ -0,0 +1,20 @@
+// author : 颜洪毅
+// e-mail : yhyzgn@gmail.com
+// time   : 2023-08-25 16:40
+// version: 1.0.0
+// desc   :
+
+package lexer
+
+import "testing"
+
+func TestToken(t *testing.T) {
+	tokens := []token{Identifier, If, And, When, AddAssign, DotDotDot}
+	wants := []string{"IDENTIFIER", "if", "&", "when", "+=", "..."}
+
+	for i, tk := range tokens {
+		if tk.String() != wants[i] {
+			t.Errorf("Want: %s, but found: %s", tk.String(), wants[i])
+		}
+	}
+}
